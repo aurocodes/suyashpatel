58a633956dd03277572bced09f4d0d81e6c56d62
adarshcode <adgupta7318@gmail.com>
2021-10-06 23:20:45 +0530

    Added unlike button and fixed the issue with the count

--- blog/blog.js
+++ blog/blog.js
@@ -1,11 +1,19 @@
-var like = 0
-var unlike = 0
+var like = 0 ,like1 = 0
+var unlike = 0, unlike1 = 0
 
 function Count(){
     document.getElementById("OUTPUT").innerHTML = "LIKE " + (like++ + 1);
 }
 
+function Count1(){
+    document.getElementById("OUTPUTS").innerHTML = "LIKE " + (like1++ + 1);
+}
+
 function UnCount(){
     document.getElementById("OUTPUT1").innerHTML = "UNLIKE " + (unlike++ + 1);
 }
- 
+
+function UnCount1(){
+    document.getElementById("OUTPUTS1").innerHTML = "UNLIKE " + (unlike1++ + 1);
+}
+
